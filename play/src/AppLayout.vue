cf4d4b2a29517ceeb1c0ebe8413ce773df433301
pangza <2331538132@qq.com>
2024-11-26 14:27:26 +0800

    submit

--- play/src/AppLayout.vue
+++ play/src/AppLayout.vue
@@ -2,9 +2,9 @@
 import { ref } from 'vue';
 
 const HeaderRender = () => <div>啊萨达萨达</div>
-const LogoRender = () => <div style={{background: 'red'}}>lOGO</div>
-const AsideRender = () => <div style={{width: '220px'}}>菜单</div>;
-const FooterRender = () => <div style={{width: '220px'}}>菜单</div>;
+const LogoRender = () => <div style={{ background: 'red' }}>lOGO</div>
+const AsideRender = () => <div style={{ width: '220px' }}>菜单</div>;
+const FooterRender = () => <div style={{ width: '220px' }}>菜单</div>;
 
 const coll = ref(false);
 
@@ -37,10 +37,16 @@ const routes = [
     }
 ]
 
+function handleMenuClick(path: string) {
+    console.log('path', path);
+}
+
 </script>
 
 <template>
-    <ProLayout routeGroup menuTheme="dark" :footerRender="FooterRender" v-model:collapsed="coll" :routes :headerRender="HeaderRender" :logoRender="LogoRender" :asideRender="AsideRender">
+    <ProLayout @menuClick="handleMenuClick" routeGroup menuTheme="dark" :footerRender="FooterRender"
+        v-model:collapsed="coll" :routes :headerRender="HeaderRender" :logoRender="LogoRender"
+        :asideRender="AsideRender">
         <div>
             Hello 我是内容
         </div>
