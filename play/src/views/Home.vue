da030ab7259117e39fb559fa5a3c9fe1fc75a1d5
pangza <2331538132@qq.com>
2024-12-05 18:12:13 +0800

    submit

--- play/src/views/Home.vue
+++ play/src/views/Home.vue
@@ -1,21 +1,34 @@
-<script setup lang='ts'>
-
+<script setup lang='tsx'>
+const header = { title: () => (<t-typography-title > tdesign-pro-component</t-typography-title>) }
 </script>
 
 <template>
-    <PageContainer :header="{ title: '我的标题' }">
-        <template #description>
-            描述111
-        </template>
+    <t-row>
+        <t-col :span="2"></t-col>
+        <t-col :span="7">
+            <PageContainer :header>
+                <template #description>
 
-        <template #actions>
-            <t-button>Hello</t-button>
-        </template>
+                    <t-typography-title level="h3">为什么开发 tdesign-pro-component</t-typography-title>
+                    <t-typography-paragraph>
+                        原因很简单，起初我是在合肥一家房地产互联网公司做开发，开发使用的是 React + Umi + Typescript 开发。其中使用到了阿里团队开发的 ProComponents
+                        组件库，发现功能挺好用的。就想开发一套这样的组件库了。一开始只是在自己的项目中引入使用，后面发现独乐乐不如众乐乐就发布到 NPM 中去了。
+                    </t-typography-paragraph>
 
-        <div>
-            内容区间
-        </div>
-    </PageContainer>
+                    <t-typography-title level="h3">为什么是 Vue 而不是 React?</t-typography-title>
+                    <t-typography-paragraph>
+                        因为作者个人是比较喜欢 Vue 框架的，当然 React 也会。所以出于个人喜好，想在 Vue 框架里面开发一套这样的组件库。
+                    </t-typography-paragraph>
+
+                </template>
+
+                <div>
+                    内容区间
+                </div>
+            </PageContainer>
+        </t-col>
+        <t-col :span="2"></t-col>
+    </t-row>
 </template>
 
 <style lang='scss'></style>
